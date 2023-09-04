92ac4c2f8f37eccfc2887ddfce79dc6716f885b5
Nicolás Vega Jara <nicolasv@kranio.io>
2023-09-04 15:19:31 -0300

    Generating full pdf with styles based on template

--- src/main/java/com/bluex/manifiesto/pdf/service/PdfService.java
+++ src/main/java/com/bluex/manifiesto/pdf/service/PdfService.java
@@ -4,9 +4,14 @@ import java.io.IOException;
 
 import org.springframework.ui.Model;
 
+import com.bluex.manifiesto.pdf.models.Manifiesto.Manifiesto;
 import com.lowagie.text.DocumentException;
 
 public interface PdfService {
   public abstract String generatePdfContent(Model model);
-  void convertHtmlToPdf(String htmlContent, String base64Image, String outputPath) throws IOException, DocumentException;
+
+  void convertHtmlToPdf(String htmlContent, String base64Image, String outputPath)
+      throws IOException, DocumentException;
+
+  void fillTemplate(Model model, Manifiesto manifest);
 }
--- src/main/java/com/bluex/manifiesto/pdf/service/impl/PdfServiceImpl.java
+++ src/main/java/com/bluex/manifiesto/pdf/service/impl/PdfServiceImpl.java
@@ -43,8 +43,6 @@ public class PdfServiceImpl implements PdfService {
   // private static final Logger logger =
   // LoggerFactory.getLogger(TransbankEquipoController.class);
 
-
-
   @Override
   public String generatePdfContent(Model model) {
     ObjectMapper objectMapper = new ObjectMapper();
@@ -53,14 +51,14 @@ public class PdfServiceImpl implements PdfService {
       Resource resource = resourceLoader.getResource("classpath:static/manifiesto-medium.json");
       InputStream inputStream = resource.getInputStream();
       InputStreamReader jsonFile = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-      // File jsonFile = ResourceUtils.getFile("classpath:static" + File.separator + "manifiesto.json");
+
       Manifiesto manifest = objectMapper.readValue(jsonFile, Manifiesto.class);
 
       System.out.println("Document ID: " + manifest.getDocument_id());
       System.out.println("Os list: " + manifest.getOs_list()[0]);
       System.out.println("Os list: " + manifest.getOs_list()[0].getOs_id());
 
-      model.addAttribute("document_id", manifest.getDocument_id());
+      fillTemplate(model, manifest);
 
       jsonFile.close();
       inputStream.close();
@@ -68,20 +66,10 @@ public class PdfServiceImpl implements PdfService {
       e.printStackTrace();
     }
 
-    // ClassLoaderTemplateResolver templateResolver = new
-    // ClassLoaderTemplateResolver();
-    // templateResolver.setSuffix(".html");
-    // templateResolver.setTemplateMode(TemplateMode.HTML);
-
-    // TemplateEngine templateEngine = new TemplateEngine();
-    // templateEngine.setTemplateResolver(templateResolver);
-
     Context context = new Context();
     context.setVariables(model.asMap());
     context.setVariable("image", ServiceConstants.TEMPLATE_BASE64_IMAGE);
     String htmlContent = templateEngine.process("index", context);
-
-    // System.out.println(htmlContent);
 
     String filePath = "file.html";
 
@@ -109,21 +97,18 @@ public class PdfServiceImpl implements PdfService {
     return "PDF content";
   };
 
+  @Override
+  public void fillTemplate(Model model, Manifiesto manifest) {
+    model.addAttribute("document_id", manifest.getDocument_id());
+    model.addAttribute("retiro_id", manifest.getRetiro_id());
+    model.addAttribute("courier", manifest.getCourier());
+    model.addAttribute("carga", manifest.getCarga());
+    model.addAttribute("client", manifest.getClient());
+    model.addAttribute("os_list", manifest.getOs_list());
+  }
+
   public void convertHtmlToPdf(String htmlContent, String base64Image, String outputPath)
       throws IOException, DocumentException {
-
-    // try (FileOutputStream fileOutputStream = new FileOutputStream(outputPath)) {
-    // ITextRenderer renderer = new ITextRenderer();
-    // SharedContext sharedContext = renderer.getSharedContext();
-    // sharedContext.setPrint(true);
-    // sharedContext.setInteractive(false);
-
-    // renderer.setDocumentFromString(htmlContent);
-    // renderer.layout();
-    // renderer.createPDF(fileOutputStream, false);
-    // renderer.finishPDF();
-    // }
-
     try (FileOutputStream fileOutputStream = new FileOutputStream(outputPath)) {
       Document document = Jsoup.parse(htmlContent);
 
@@ -136,24 +121,5 @@ public class PdfServiceImpl implements PdfService {
       builder.run();
 
     }
-
-    // try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-    // PdfWriter writer = new PdfWriter(outputStream);
-
-    // DefaultFontProvider defaultFont = new DefaultFontProvider(false, true,
-    // false);
-
-    // ConverterProperties converterProperties = new ConverterProperties();
-    // converterProperties.setFontProvider(defaultFont);
-
-    // HtmlConverter.convertToPdf(htmlContent, writer, converterProperties);
-
-    // FileOutputStream outputFile = new FileOutputStream(outputPath);
-    // outputStream.writeTo(outputFile);
-    // outputStream.close();
-
-    // outputStream.flush();
-    // outputFile.close();
-    // }
   }
 }
